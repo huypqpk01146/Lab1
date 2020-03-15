637e8571a0b359dc9d54e0f3db09f3522d62f496
huypqpk01146 <phanhuy121096@gmail.com>
2020-03-15 20:34:04 +0700

    3 files modified, 1 added

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './Product';
 
 @Component({
   selector: 'app-product',
@@ -11,5 +12,14 @@ export class ProductComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  product : Product = {
+    name: "Sản phẩm 1",
+    price: 26.99,
+    desc: "Thông tin mô tả sản phẩm",
+    img: "http://placehold.it/700x400",
+    status: true
+  }
+  changeStatus(){
+    this.product.status = false; 
+  }
 }
